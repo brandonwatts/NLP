9875b89f1e686d7d3019fa154a3d26da36df92a0
Brandon Watts <brandonwatts@outlook.com>
2017-02-20 10:29:47 -0500

    Fix c

--- submission.c
+++ submission.c
@@ -2,83 +2,174 @@
 #include <stdlib.h>
 
 
-/* foo.h
-
-#ifndef FOO_H_   /* Include guard */
-/*
-#define FOO_H_
-
-int foo(int x);  /* An example function declaration */
-/*
-#endif // FOO_H_
-foo.c
-
-#include "foo.h"  /* Include the header (not strictly necessary here) */
-/*
-int foo(int x)    /* Function definition */
-//{
-//    return x + 5;
-//}
-//main.c
-
-//#include <stdio.h>
-///#include "foo.h"  /* Include the header here, to obtain the function declaration */
-
-//int main(void)
-//{
- //   int y = foo(3);  /* Use the function here */
-  //  printf("%d\n", y);
-   // return 0;
-//}
-//To compile using GCC
-
-//gcc -o my_app main.c foo.c
-struct image {
-	int width;
-	int height;
-	int** cells; 
-}
-
-struct video {
-	struct image val;
-    struct node * next;
-} 
-
+/**  Method that will create a white image of a specified size.
+*
+*  @param width   Width of our image we wish to create.
+*  @param height  Height of our image we wish to create.
+*  @return 		  Pointer to the image.
+**/
 struct image* createWhiteImg(unsigned int width,unsigned int height) {
+
+	/** Allocate a 2-D array with calloc so it initializes all cells to 0 **/
+	int** array = calloc(width,sizeof(int*));
+
+	int i;
+
+	/** Interate over each row and Allocate it a hieght **/
+	for(i=0;i<width;i++) array[i] = calloc(height, sizeof(int));
+
+	/** Make the image white **/
+	int k,j;
+	for(k = 0; k < width; k++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			array[k][j] = 255;
+		}
+	}
+
+	/** Allocate enough space for our image **/
+	struct image* img = malloc(sizeof(struct image));
+
+	/** Initialize all the image member variables **/
+	img->cells = array;
+	img->width = width;
+	img->height = height;
+
+	return img;
+}
+
+/**  Method that set a given pixel of a specified Image.
+*
+*  @param img     	 Pointer to the specifeid image.
+*  @param x  		 X position of our pixel.
+*  @param y			 Y position of our pixel.
+*  @param greyscale  Greyscale value we wish to give the image.
+**/
+void setPixel( struct image* img, unsigned int x,unsigned int y,unsigned char grayscale) {
+		img->cells[x][y] = grayscale;
+}
+
+/**  Method that will retrieve the Greyscale value of a specific pixel.
+*
+*  @param img  Pointer to the specified image.
+*  @param X    X Position of our pixel.
+*  @param Y    Y position of our pixel.
+*  @return     Greyscale value at that point.
+**/
+unsigned char getPixel( struct image* img,unsigned int x,unsigned int y) {
+   	return img->cells[x][y];
+}
+
+/**  Method that will create a video.
+*
+*  @return  Pointer to the video.
+**/
+struct video* createEmptyFrameList() {
+
+	/** Allocate a video on the heap **/
+	struct video *vid = malloc(sizeof(struct video));
+
+	/** Allocate a "blank frame" **/
+	struct frame* imgFrame = malloc(sizeof(struct frame));
+
+	/** Populate our "blank frame" **/
+	imgFrame -> img = NULL;
+	imgFrame -> next = NULL;
+
+	/** Set the video length to 0 **/
+	vid->length = 0 ;
+
+	return vid;
+}
+
+/**  Method that insert a frame to the front of our video.
+*
+*  @param video  Pointer to the specified video.
+*  @param img    Image we wish to insert.
+*  @return       Pointer to our video.
+**/
+struct video* insertFrameUpFront( struct video* video, struct image* img) { 
+
+	/** Allocate an imageFrame on the Heap **/
+	struct frame* imgFrame = malloc(sizeof(struct frame));
+
+	/** Increse the length of our video **/
+	video->length++;
+
+	/** Set the frames img resource **/
+	imgFrame->img = img;
+
+	/** Set the frames next pointer to point at the first frames next pointer **/
+	imgFrame->next = video->head;
+
+	/** Set the head to our current frame **/
+	video->head = imgFrame;
+
+	return video;
+}
+
+/**  Method that return the Nth frame in the video and will return NULL if such a frame does not exist.
+*
+*  @param video    Pointer to the specified video.
+*  @param frameNo  Number of the frame we wish to delete.
+*  @return 		   Pointer to the image contained by the frame.
+**/
+struct image* getNthFrame(struct video* video, int frameNo){
+
+		/** Set the current frame to the first frame of the video **/
+		struct frame* current = video->head;
+
+		int i;
+
+		/** Iterate over the frames an look for the frame. If there arent enough frames, we just return null **/
+	 	for(i=0;i < frameNo;i++) {
+
+	 		if(current->next == NULL)
+	 			return NULL;
+
+	 		current = current->next;
+	 	}
+
+	 	/** We are just returning the node, no pointers to update. **/
+	 	return current->img;
+}
+
+/**  Method that delete a specified frame. It returns null if such a frame does not exist.
+*
+*  @param video   Pointer to the video.
+*  @param frameNo The Frame Number we wish to delete.
+*  @return 		  Pointer to the Video after deletion of the frame.
+**/
+struct video* deleteNthFrame( struct video* video, int frameNo) {
 	
-	int i, **array;
-	array = malloc(width*sizeof(int*));
-	for(i=0;i<width;i++) array[i] = malloc(height*sizeof(int));
+	/** We use a temp node to update pointers **/
+	struct frame* tempFrame = NULL;
 
-	struct image img = malloc(sizeof(image));
-	img.cells = array;
-	img.width = width;
-	img.height = height;
+	/** Set the current frame to the first frame of the video **/
+	struct frame* current = video->head;
 
-	return &img;
+	int i;
 
-}
+	/** Iterate over the frames an look for the frame. If there arent enough frames, we just return null **/
+	for(i=0;i < frameNo-1;i++) {
+	 		
+		if(current->next == NULL)
+	 			return NULL;
+	 	current = current->next;
+	}
 
-void setPixel( struct image* img,un signed int x,unsigned int y,unsigned char grayscale) {
+	/** Set our temp node pointer to the current frames next frame. **/
+	tempFrame = current->next;
 
-}
+	/** Set the currents frames pointer to go around tempFrame **/
+	current->next = tempFrame->next;
 
-unsigned char getPixel( struct image *img,unsigned int x,unsigned int y) {
+	/** Decrease the videos frames by 1 **/
+	video->length--;
 
-}
+	/** Free that frame so we dont get memory leaks **/
+	free(tempFrame);
 
-struct video *createEmptyFrameList() {
-
-}
-
-struct video *insertFrameUpFront( struct video *video, struct image *img) { 
-
-}
-
-struct image *getNthFrame( struct video *video, int frameNo){
-
-}
-
-struct video *deleteNthFrame( struct video *video, int frameNo) {
-
+	return video;
 }
--- test.c
+++ test.c
@@ -2,11 +2,22 @@
 #include <stdio.h>
 #include "submission.h"
 
+void printImageVals(struct image* img,int width,int height) {
+	 int i,j;
+	 for (i = 0; i < width; i++) {
+      for (j = 0; j < height; j++) {
+            printf("%d ", img->cells[i][j]);
+        }
+        printf("\n");
+    }
+}
+
  int main() {
  struct image *a=createWhiteImg(16,9);
  struct image *b=createWhiteImg(16,9);
  struct image *c=createWhiteImg(16,9);
- /*setPixel(a,0,0,14);
+ setPixel(a,0,8,14);
+ printImageVals(a,16,9);
  setPixel(b,15,8,28);
  setPixel(c,10,4,54);
  struct image *d=NULL;
@@ -17,9 +28,14 @@
  v =deleteNthFrame(v,1);
  d =getNthFrame(v,1);
  unsigned char px=getPixel(d,10,4);
- if (px==54)
+if (px==54)
  	printf("WORKS\n");
  else
- 	printf("BUMMER\n");
- return 0;*/
+ 	printf("BUMMER PX is: %d\n",px);
+ 	
+ return 0;
+
 }
+
+
+
