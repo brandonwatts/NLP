dd6849e90b2420522bf76a2b1a4ceec851e1da39
Brandon Watts <brandonwatts@outlook.com>
2017-02-24 14:35:45 -0500

    add scorer and fix tagger

---
+++ Assignment 3/scorer.pl
@@ -0,0 +1,82 @@
+#! usr/bin/perl
+
+######### INFORMATION ########
+
+# Brandon Watts
+# CMSC 416
+# Assignment 3
+# 2/22/17
+
+use warnings;
+use feature qw(say switch);
+use File::Slurp;
+use strict;
+
+### Training data file ###
+my $taggedFile = read_file($ARGV[0]);
+
+### Test data file ###
+my $key = read_file($ARGV[1]);
+
+my @tokensForKey;
+    my @POSForKey;
+    my @tokensForTagged;
+    my @POSForTagged;
+
+processKey($key);
+processTagged($taggedFile);
+computeAccuracy();
+   
+sub processKey {
+
+    my $rawData = $_[0];
+    $rawData =~ s/\[|\]//g;
+    $rawData =~ s/\s+|_/ /g; # Delete all of the Tabs and remove extra Spaces.
+    $rawData =~ s/^\s+|\s+$//g; # Trim the file
+
+    my @sentenceTokens = $rawData=~ /[:\.,\w+]+\/[\w:,\.:]+/g;    
+
+    foreach my $i (0.. $#sentenceTokens ) {
+
+       if ($sentenceTokens[$i] =~ /([:\.,\w+]+)\/([\w:,\.:]+)/) { 
+
+        $tokensForKey[$i] = $1; 
+        $POSForKey[$i] = $2;
+       } 
+    }
+}
+
+sub processTagged {
+    my $rawData = $_[0];
+
+    my @sentenceTokens = $rawData=~ /[:\.,\w+]+\/[\w:,\.:]+/g;
+    
+    foreach my $i (0.. $#sentenceTokens ) {
+
+       if ($sentenceTokens[$i] =~ /([:\.,\w+]+)\/([\w:,\.:]+)/) { 
+
+        $tokensForTagged[$i] = $1; 
+        $POSForTagged[$i] = $2;
+       } 
+    }
+}
+
+sub computeAccuracy {
+
+    my $true = 0;
+    my $false = 0;
+
+    foreach my $i (0.. $#POSForTagged ) {
+
+       if ($POSForTagged[$i] eq $POSForKey[$i]) { 
+                $true++;
+       } else {
+                $false++;
+       }
+    }
+
+    print("TRUE: $true");
+    print("FALSE: $false")
+}
+
+
--- Assignment 3/tagger.pl
+++ Assignment 3/tagger.pl
@@ -12,12 +12,19 @@ use feature qw(say switch);
 use File::Slurp;
 use strict;
 
-
-
+### Holds the frequency count of all the POS tags ###
 my %frequencyTable;
+
+### Double hash of word to POS tag mapped to count ###
 my %wordToPOSMapping;
+
+### Double hash of POS tag to previous POS tag mapped to count ###
 my %POStoPOSMapping;
+
+### Training data file ###
 my $trainingData = read_file($ARGV[0]);
+
+### Test data file ###
 my $testData = read_file($ARGV[1]);
 
 
@@ -28,19 +35,92 @@ preprocess($trainingData);
     generateTaggedFile($testData);
 
 
-sub generateTaggedFile {
+
+#  Method that initializes the program 
+#
+#  @param $_[0]  Will hold the Training data file in its entirety
+sub preprocess {
+
+    my $rawData = $_[0];
+    $rawData =~ s/\[|\]//g;
+    $rawData =~ s/\s+|_/ /g; # Delete all of the Tabs and remove extra Spaces.
+    $rawData =~ s/^\s+|\s+$//g; # Trim the file
+
+    createFrequencyTable($rawData);
+    createWordtoPOSMapping($rawData);
+    createPOStoPOSMapping($rawData);
+}
+
+#  Method that maps all the POS tags the the frequency in which they occur in the file
+#
+#  @param $_[0]  Will hold the Training data file in its entirety
+sub createFrequencyTable {
+
     my $file = $_[0];
-    $file =~ s/\[|\]//g;
-    my @sentenceTokens = $file=~ /\b[A-Z]+[a-z]+\b|\d*\.\d*/g;
-    my @sentenceTokensPOS;
+    my @POS = $file=~ /[:\.,\w+]\/([\w:,\.:]*)/g; # Grab all the POS tags, semicolons, commas, and periods and place them in an array
+       
+
+    ### Iterate over that array and count the number of times a tag has been seen ###
+    for my $pos (@POS) { $frequencyTable{$pos}++; }
+}
+
+#  Method that maps all the words/tokens with thier respective POS tag
+#
+#  @param $_[0]  Will hold the Training data file in its entirety
+sub createWordtoPOSMapping {
+    my $data = $_[0];
+    my @sentenceTokens = $data=~ /[\w+:,\.]*\/[\w+:,\.]*/g;
+
+    for my $token (@sentenceTokens) {
+
+        ### Seperate the word from the POS into 2 capture groups ###
+         if ($token =~ /([:\.,\w+]*)\/([\w:,\.:]*)/) { $wordToPOSMapping{$1}{$2}++; }
+    }
+}
+
+#  Method that maps all the POS tags with thier previous POS tag
+#
+#  @param $_[0]  Will hold the Training data file in its entirety
+sub createPOStoPOSMapping {
+    my $rawData = $_[0];
+
+    ### Regex to grab just the POS tag in a capture group ###
+    my @sentenceTokens = $rawData=~ /[:\.,\w+]\/([\w:,\.:]*)/g;
+    
+           #print join("\n",@sentenceTokens),"\n";
+
+
 
     foreach my $i (1.. $#sentenceTokens ) {
-        getPOS($sentenceTokens[$i]); 
+        $POStoPOSMapping{$sentenceTokens[$i]}{$sentenceTokens[$i-1]}++;
+    }
+}
+
+#  Method that generates the tagged output file 
+#
+#  @param $_[0]  Will hold the Trst data file in its entirety
+sub generateTaggedFile {
+
+    my $file = $_[0];
+    $file =~ s/\[|\]//g;
+    $file =~ s/\s+|_/ /g; # Delete all of the Tabs and remove extra Spaces.
+    my $outputFile = "pos-test-with-tags.txt";
+
+
+
+    my @sentenceTokens = $file=~ /\b\w+\b|[\.,:]/g;
+    my @sentenceTokensPOS;
+
+    foreach my $i (0.. $#sentenceTokens ) {
+              my $POSGuess = getPOS($sentenceTokens[$i]);
+       append_file($outputFile, "$sentenceTokens[$i]/$POSGuess\n"); 
     }
 
 }
 
-
+#  Method that get a POS tag given a word and its previous POS
+#
+#  @param $_[0]   
 sub getPOS {
     my $currentWord = $_[0];
     my $max;
@@ -50,64 +130,40 @@ sub getPOS {
        if(exists $wordToPOSMapping{$currentWord}) {
             $max = 0;
             for my $value ( keys %{ $wordToPOSMapping{$currentWord} } ) {
-                if($wordToPOSMapping{$currentWord}{$value} > $max) {
-                        $max = ($wordToPOSMapping{$currentWord}{$value}/$frequencyTable{$value})*($POStoPOSMapping{$prevPOS}{$value}/$frequencyTable{$value});
+
+                my $frequencyTableValue;
+                my $POSMapping;
+                my $currentMap;
+
+                if(exists $frequencyTable{$value}) {
+                    $frequencyTableValue =  $frequencyTable{$value};
+                } else {
+                    $frequencyTableValue = 1;
+                }
+
+                if(exists $POStoPOSMapping{$value}{$prevPOS}) {
+                   $POSMapping = $POStoPOSMapping{$value}{$prevPOS};
+                } else {
+                    $POSMapping = 0;
+                }
+
+
+                if(exists $wordToPOSMapping{$currentWord}{$value}) {
+                   $currentMap = $wordToPOSMapping{$currentWord}{$value};
+                } else {
+                    $currentMap = 0;
+                }
+
+
+                my $argMax = ($currentMap/$frequencyTableValue)*($POSMapping/$frequencyTableValue);
                         $POS = $value;
+                if($argMax > $max) {
+                        $max = $argMax;
                 }
             }
-            print $POS."\n";
+            return $POS;
         }
-        else {
-            print "THERE ARE NO KEYS\n"
-        }
-        
-}
-
-
-
-
-sub preprocess {
-    my $rawData = $_[0];
-    $rawData =~ s/\s+|_/ /g; # Delete all of the Tabs and remove extra Spaces.
-    $rawData =~ s/\[|\]//g;
-    
-    $rawData =~ s/^\s+|\s+$//g;
-    createFrequencyTable($rawData);
-    createWordtoPOSMapping($rawData);
-    createPOStoPOSMapping($rawData);
-    
-}
-
-sub createWordtoPOSMapping {
-    my $data = $_[0];
-    my @sentenceTokens = $data=~ /\w+\/\w+/g;
-
-    for my $token (@sentenceTokens)
-    {
-         if ($token =~ /(\w+)\/([A-Z]+|[\.,])/) {
-                $wordToPOSMapping{$1}{$2}++;
-        }
-    }
-
-}
-
-sub createPOStoPOSMapping {
-    my $rawData = $_[0];
-    my @sentenceTokens = $rawData=~ /[\w\.,]\/(\w+)/g;
-    foreach my $i (1.. $#sentenceTokens ) {
-        $POStoPOSMapping{$sentenceTokens[$i]}{$sentenceTokens[$i-1]}++;
-    }
-}
-
-sub createFrequencyTable {
-
-    my $file = $_[0];
-    my @POS = $file=~ /\/([A-Z]+|[\.,])/g;
-       
-       #print join("\n",@POS),"\n";
-
-    for my $pos (@POS) {
-        $frequencyTable{$pos}++;}
+        else { return "NN"; }
 }
 
 sub printFrequecyTable {
