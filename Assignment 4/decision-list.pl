30c5a1b19283d472d54ce70a6dcf681f994430f9
Brandon Watts <brandonwatts@outlook.com>
2017-03-22 16:17:57 -0400

    fix the decision list and add scorer

--- Assignment 4/decision-list.pl
+++ Assignment 4/decision-list.pl
@@ -7,87 +7,37 @@
 # Assignment 3 - Tagger
 # 3/2/17
 
-######## SUMMARY #########
-
-# This program attempts to perform POS (Part of Speech) tagging. It learns of of the training data set file pos-train.txt. From 
-# there it will read in the file pos-test.txt and attempt to POS tag the tokens within that file. We then specify an output file 
-# with the > charcter to insist on where the output file should be witten to. 
-
-######## ACCURACY AND RULES ########
-
-# Becuase my program also used the previous tag to make its calculations, my accuray was able to climb to about a 90.5%
-#
-# I still added a few basic grammar rules so I wouldnt make the assumption that a word I hadnt seen was always a Noun.
-#
-# 1) If the word contains a hypen, it usually, but not always, represents an acjective so lets just assume it is.                      
-# 2) Assume that most adverbs end in ly             
-# 3) Since we are assuming a noun (singular), adding an 's' would give us a noun plural.
-# 4) Past tense verbs most of the time end in 'ed' 
-# 5) If we see an interjection, just label it 
-# 6) If the word ends in 'ing' there is a good chance its a verb
-
-########## EXAMPLE USE CASE #########
-
-# $ perl tagger.pl pos-train.txt pos-test.txt > pos-test-with-tags.txt
-#
-# The first arguement is the file in which our training data is stored and the second file is the test data. The las arguement is the file which
-# you want the output written to.
-
-######### ALGORITHIMS ########
-
-# Algorithims are decribed in detail below but the idea is that the file pos-train.txt is split into its respective tokens. From there
-# we are creating 3 different hashes : A hash called frequencyTable that holds the frequency of our POS tags, A double hash called wordToPOSMapping
-# that hold a word to its POS tag mapped to the count, and a hash called POStoPOSMapping which holds a tag to its previous tag mapped to the count.
-#
-#   1) Process the training data by deleting all of the brackets, Deleting all the extra spaces, new line characters, and tabs
-#      and replacing them with a single space.
-#   2) We then create our Frequencytable, our WordToPOSMapping, and our training data, respectively.
-#   3) From there we can begin to start generating our tagged file by looping through all the tokens in our test file,
-#      running our getPOS() funtion on it, and then print it to the file pos-test-with-tags.txt
-#   4) Our getPOS() function works by first checking if our word has been seen before.
-#   5) If the word has been seen before, We compute the P(w,t) * P(t,t-1)  ** Probablity of the word given the tag times the
-#      Probablity if the tag given the previous tag. ** The highest value is our POS guess.
-#   6) If we have not seen the word before we attempt to apply some rules to figure out what the word is, but if all else fails, we will just call it a NNP (Proper Noun)
-
-########## REFERENCES #########
-
-# "Regular Expressions Cheat Sheet" - https://www.cheatography.com/davechild/cheat-sheets/regular-expressions/
-# "Part-of-speech tagging" - https://en.wikipedia.org/wiki/Part-of-speech_tagging
-# "File::Slurp" - http://search.cpan.org/~uri/File-Slurp-9999.19/lib/File/Slurp.pm
-
 use warnings;
 use feature qw(say switch);
-#use strict;
 
 use File::Slurp;
-use Data::Dumper;
 use XML::Simple;
 use List::Util;
 
 my %frequencyTable;
 
+my %topFrequencies;
+
 my $data;
 
+my %wordTypes;
 
-### Training data file ###
+my %guessedSenses;
+
+my %wordsSurrounding;
+
 my $trainingData = read_file($ARGV[0]);
 
-### Test data file ###
 my $testData = read_file($ARGV[1]);
 
-### Test data file ###
-my $decisionList = read_file($ARGV[2]);
 
-preprocess($trainingData);
-#printFrequecyTable();
+preprocess($trainingData, $testData);
 
-#  Method that initializes the program 
-#
-#  @param $_[0]  Will hold the Training data file in its entirety
 sub preprocess {
 
-### Training data file ###
-my $train = $_[0];
+    my $train = $_[0];
+
+    my $test = $_[1];
 
 ### Delete all of the brackets from the input file ###
     $train =~ s/<@>//g;
@@ -95,35 +45,39 @@ my $train = $_[0];
     $train =~ s/<\/p>//g;
     $train =~ s/&//g;
 
+### Delete all of the brackets from the input file ###
+    $test =~ s/<@>//g;
+    $test =~ s/<p>//g;
+    $test =~ s/<\/p>//g;
+    $test =~ s/&//g;
 
    # create object
-my $xml = new XML::Simple;
+    my $xml = new XML::Simple;
 
-write_file("ex.txt",$train);
-# read XML file
-$data = $xml->XMLin($train);
+    write_file("ex.txt",$train);
+    # read XML file
+    $data = $xml->XMLin($train);
 
-write_file 'mydump.log', Dumper($data);
+    $testData = $xml->XMLin($test);
 
-createFeatures();
-printFrequecyTable();
+    createFeatures();
+    createWordTypes();
+    createWordsSurrounding("phone");
+    createWordsSurrounding("product");
+    computeSenseID();
+    createAnswerFile();
 }
-
 
 #  Method that generates the tagged output file 
 #
 #  @param $_[0]  Will hold the Test data file in its entirety
 sub createFeatures {
-        my @surroundingWords;
+    
+    my @surroundingWords;
 
-     for $instance ( keys %$data->{lexelt}->{instance} ) {
-
-
-        if (ref($data->{lexelt}->{instance}->{$instance}->{context}->{'s'}) eq "HASH"){
-           foreach my $surroundingWords ($data->{lexelt}->{instance}->{$instance}->{context}->{'s'}->{content}){
-                push(@surroundingWords,$surroundingWords);
-            } 
-        } else {
+    for $instance ( keys %$data->{lexelt}->{instance} ) {
+        else {
+            
             foreach my $part ($data->{lexelt}->{instance}->{$instance}->{context}->{'s'} ) {
                     foreach my $p (@$part){
                             if(ref($p) eq "HASH"){
@@ -140,14 +94,173 @@ sub createFeatures {
             }
         }
     }
+
     my $allWords = join(' ',@surroundingWords);
+
     $allWords =~ s/\s+|_/ /g; # Delete all of the Tabs and remove extra Spaces.
 
     my @words = $allWords=~ /\S+/g;
-     foreach my $word (@words) {
-        $frequencyTable{$word}++; 
+    foreach my $word (@words) { $frequencyTable{$word}++;}
+}
+
+sub createWordsSurrounding {
+       
+    my $senseid = $_[0];
+
+    for $instance ( keys %$data->{lexelt}->{instance} ) {
+
+        if ($data->{lexelt}->{instance}->{$instance}->{answer}->{senseid} eq $senseid){
+           
+           if (ref($data->{lexelt}->{instance}->{$instance}->{context}->{'s'}) eq "HASH"){
+           
+                foreach my $surroundingWords ($data->{lexelt}->{instance}->{$instance}->{context}->{'s'}->{content}){
+                    push(@surroundingWords,$surroundingWords);
+                }
+            }    
+            else {
+                foreach my $part ($data->{lexelt}->{instance}->{$instance}->{context}->{'s'} ) {
+                    foreach my $p (@$part){
+                            if(ref($p) eq "HASH"){
+                                foreach my $surroundingWords ($p->{content}){
+                                   foreach my $s (@$surroundingWords){
+                                        push(@surroundingWords,$s);
+                                    }
+                                }
+                            }
+                            else{
+                               push(@surroundingWords,$p);
+                            }
+                    }
+                }
+             }
+         }
+     }
+
+     my $allWords1 = join(' ',@surroundingWords);
+     $allWords1 =~ s/\s+|_/ /g; # Delete all of the Tabs and remove extra Spaces.
+
+     $wordsSurrounding{$senseid} = $allWords1;
+}
+
+sub createWordTypes {
+
+     for $instance ( keys %$data->{lexelt}->{instance} ) {
+
+          my $senseid = $data->{lexelt}->{instance}->{$instance}->{answer}->{senseid};
+          $wordTypes{$senseid}++;
+    } 
+}
+
+sub getTimesWordOccuredWithFeature {
+    my $searchWord = $_[0];
+    my $senseid = $_[1];
+
+    $wordsSurroundingFeature = $wordsSurrounding{$senseid};
+    $wordsSurroundingFeature =~ s/\s+|_/ /g; # Delete all of the Tabs and remove extra Spaces.
+
+    my @individualWords = $wordsSurroundingFeature=~ /\S+/g;
+    my $counter = 0;
+     foreach my $word (@individualWords) {
+        if($word eq $searchWord){
+            $counter++;
+        } 
+    }
+    return $counter;
+}
+
+sub computeSenseID {
+        
+    my $max;
+    my $allWords = ();
+    my @words =();
+    my @surroundingWords = ();
+    my @types = ("phone","product");
+    my $guessedSense = "UNDETERMINED";
+
+    ########## For Every instance ##########
+    for $instance ( keys %$testData->{lexelt}->{instance}) {
+        
+        ########## Empty arrays from the previous instance  ##########
+        $allWords = ();
+        @words =();
+        @surroundingWords = ();
+
+        ########## The 2 senses the word "line" can have is "product" and "phone" ##########
+        @types = ("product","phone");
+
+        ########## Set the default sense to undetermined for easier debugging if a sense is not chosen ##########
+        $guessedSense = "UNDETERMINED";
+
+        ########## 's' can either be a hash or an array from the xml parser (not really sure why) so I have to check so I can access it the correct way ##########
+        if (ref($testData->{lexelt}->{instance}->{$instance}->{context}->{'s'}) eq "HASH"){
+
+           foreach my $surroundingWords ($testData->{lexelt}->{instance}->{$instance}->{context}->{'s'}->{content}){
+                    push(@surroundingWords,$surroundingWords);
+                };
+        }
+        ########## 's' was an array ########## 
+        else {
+            foreach my $part ($testData->{lexelt}->{instance}->{$instance}->{context}->{'s'} ) {
+                    foreach my $p (@$part){
+                            if(ref($p) eq "HASH"){
+                                foreach my $surroundingWords ($p->{content}){
+                                   foreach my $s (@$surroundingWords){
+                                        push(@surroundingWords,$s);
+                                    }
+                                }
+                            }
+                            else{
+                               push(@surroundingWords,$p);
+                            }
+                    }
+            }
+        }
+
+        $allWords = join(' ',@surroundingWords);
+        $allWords =~ s/\s+|_/ /g; # Delete all of the Tabs and remove extra Spaces.
+        @words = $allWords=~ /\S+/g;
+        my $max = 0;
+        my $typeScore;
+        foreach my $type (@types){
+            $typeScore =1;
+            foreach my $word (@words) {
+
+                my $timesWordAppearedinTrainingData = $wordTypes{$type};
+
+                my $TimesWordOccuredWithFeature = getTimesWordOccuredWithFeature($word,$type);
+
+                if($TimesWordOccuredWithFeature == 0){
+                    next;
+                }
+                $typeScore += log($TimesWordOccuredWithFeature/$timesWordAppearedinTrainingData);
+            } 
+
+            if (abs($typeScore) > $max) {
+                $max = abs($typeScore);
+                $guessedSense = $type;
+            }
+        }
+ 
+        $guessedSenses{$instance} = $guessedSense;
+    }
+
+}
+
+sub createAnswerFile {
+    foreach my $key (keys %guessedSenses) {    # Loop through the dictionary
+        printf(STDOUT "<answer instance=\"$key\" senseid=\"$guessedSenses{$key}\"/>\n");
     }
 }
+
+sub printWordTypes {
+
+    say "---------------------- Word Types ----------------------------";
+
+    foreach my $key (sort {$wordTypes{$b} <=> $wordTypes{$a} or $b cmp $a } keys %wordTypes ) {    # Loop through the dictionary
+        printf("KEY: %-25s VALUE: %-25s\n", $key, $wordTypes{$key});
+    }
+}
+
 
 sub printFrequecyTable {
 
@@ -155,6 +268,11 @@ sub printFrequecyTable {
 
     foreach my $key (sort {$frequencyTable{$b} <=> $frequencyTable{$a} or $b cmp $a } keys %frequencyTable ) {    # Loop through the dictionary
         printf("KEY: %-25s VALUE: %-25s\n", $key, $frequencyTable{$key});
+    }
+}
+sub printguess{
+       foreach my $key (keys %guessedSenses ) {    # Loop through the dictionary
+        printf("KEY: %-25s VALUE: %-25s\n", $key, $guessedSenses{$key});
     }
 }
 
---
+++ Assignment 4/scorer.pl
@@ -0,0 +1,82 @@
+#! usr/bin/perl
+
+######### INFORMATION ########
+
+# Brandon Watts
+# CMSC 416
+# Assignment 3 - Scorer
+# 3/2/17
+
+use File::Slurp;
+
+use warnings;
+use feature qw(say switch);
+#use strict;
+use XML::Simple;
+use Data::Dumper;
+
+
+### Training data file ###
+my $answers = read_file($ARGV[0]);
+
+### Test data file ###
+my $key = read_file($ARGV[1]);
+
+my %confusionMatrix;
+my $answerHash;
+my $keyHash;
+
+processKey($key);
+processAnswers($answers);
+computeAccuracy();
+   
+sub processKey{
+    my $key = $_[0];
+    my $xml = new XML::Simple;
+    $keyHash = $xml->XMLin($key);
+    #write_file 'x.txt',Dumper($keyHash);
+}
+
+sub processAnswers{
+    my $answers = $_[0];
+    my $xml = new XML::Simple;
+    $answerHash = $xml->XMLin($answers);
+    #write_file 'ax.txt',Dumper($answerHash);
+}
+
+#  Method that will compute the accuacy, create a confusion matrix and output the data to the file specified.
+sub computeAccuracy {
+
+    my %keyMapping;
+    foreach my $instances ($answerHash->{answer}){
+            foreach my $id (@$instances){
+               $keyMapping{$id->{instance}} = $id->{senseid};
+            }
+    }  
+
+    print $keyMapping{'line-n.w9_16:217:'};
+
+    my $true = 0;
+    my $false = 0;
+    my $total = 0;
+    foreach my $instances ($answerHash->{answer}){
+            foreach my $id (@$instances){
+                if(($id->{senseid}) eq ($keyMapping{$id->{instance}})) {
+                    $true++;
+                    $total++;
+                } else {
+                    $false++;
+                    $total++;
+                }
+                #say "For instance: ".$id->{instance}."I Choose: ".$id->{senseid}." The Correct answer is ". $keyMapping{$id->{instance}};
+            }
+    }
+    say "Total Correct: $true";
+    say "Total Incorrect: $false";
+    $PercentageCorrect = $true/$total;
+    $PercentageIncorrect = $false/$total;
+
+    say "Percentage Correct: ".$PercentageCorrect*100;
+    say "Percentage Incorrect: ".$PercentageIncorrect*100;
+
+}
