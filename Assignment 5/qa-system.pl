ef40cb1bf5966de65084ea19c4b4c215903a1faf
Brandon Watts <brandonwatts@outlook.com>
2017-04-24 13:20:23 -0400

    finish assignment 6

--- Assignment 5/qa-system.pl
+++ Assignment 5/qa-system.pl
@@ -1,7 +1,6 @@
 #! usr/bin/perl
 
 use warnings;
-#use strict;
 use Switch;
 use WWW::Wikipedia;
 use File::Slurp;
@@ -16,17 +15,37 @@ use String::Util 'trim';
 
 # Brandon Watts
 # CMSC 416
-# Assignment 5
-# 4/12/17
+# Assignment 6
+# 4/24/17
 
-
-#####****************** Changes for PA6 ********************#####
+######### Changes for PA6 #########
 
 # Enhancement 1 for Query Reformulation: The addition of "Variations"
-# Enhancement 2 for Query Reformulation: The addition of Tense phrases
+
+  # It was seen upon inspecting a Wiki document that a given person's name is not used throughout the text. For example "George Washigton" is only 
+  # stated a couple times whereas "George" or "Washington" may appear many times. In addition to "George Washington" I am also searching
+  # for these partial matches as well.
+
+# Enhancement 2 for Query Reformulation: The addition of Tense phrases.
+
+  # In addition to Variations I added to the ability for the query to change tense phrases. For example if you search "When is Geroge Washington's birthday?"
+  # my system converts that "is" into its different conjugations ("was","where","are","were") and searches those as well.
+
 # Enhancement 1 for Answer Composition: The addition of "Filtering"
+
+  # I found that my answer system was just stupidly picking the first sentence that matched the regex. To aliveiate this, the system picks 
+  # all sentences that matches the regex and filters out the ones that dont match the expected answer type. For example if we were searching for
+  # "When was George Washington born?" the expected answer type would be a Date and so we could eliminate everythign that was not a date.
+
 # Enhancement 2 for Answer Composition: The addition of the "Backoff Model"
 
+  # I noticed that my QA system was a bit slow because of the time that was required to get the fulltext and parse it. To alleviate this,
+  # the system first checks if the answer is in the head of the file and only if we cant find it there do we move on the the fulltext. 
+
+# Calulation of confidence scores
+  
+  # Confidence scores were calculated by multiplying a small fraction (.99) by 1 to slowly decrease the confidence the more iterations the
+  # algorithim runs. This is becase for each iteration a new backoff model is introduced which impacts our confidence score.
 
 ######## SUMMARY #########
 
@@ -43,17 +62,19 @@ use String::Util 'trim';
 
 ######### ALGORITHIMS ########
 
-#   There were really no major alogorithims used in this iteration of the QA system but I will decribe the method by which I 
-#   turned a query into a response.
-#
-#   1) I first rewrote the query into a possible answer form using the subject and the question type 
-#   2) From there I parsed the subject out of the Query and used Wikipedia to obtain its document
-#   3) If the document cannot be obtained it attempts to search through related documents but has so far been unsuccessful
-#   4) "Searching" through the document involves attempting to direct regex match the likely answer
+#   1) The query is first parsed to remove puctutation and unknown symbols
+#   2) The expected answer to the question is then obtained
+#   3) Using a NER, the subject and modifier are both obtained from the query
+#   4) The subject is then used to query Wikipedia and obtain its document
+#   5) "Searching" through the document involves trying to first find the direct regex match for the query and then slowly backing
+#       off as the answers query moves farther away from the direct answer
+#   6) Once an answer is found we assign a confidence score to it, if we cannot find an answe we print "Answer not found"
 
 ########## REFERENCES #########
 # WWW::Wikipedia - http://search.cpan.org/~bricas/WWW-Wikipedia-2.04/lib/WWW/Wikipedia.pm
 # File::Slurp - http://search.cpan.org/~uri/File-Slurp-9999.19/lib/File/Slurp.pm
+# Data::Dumeper - http://search.cpan.org/~smueller/Data-Dumper-2.161/Dumper.pm
+# Lingua::EN::NamedEntity - http://search.cpan.org/dist/Lingua-EN-NamedEntity/NamedEntity.pm
 
 my $wiki = WWW::Wikipedia->new();
 
